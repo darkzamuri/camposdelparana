5fb96c2fb7530b4468ecced31e12618e65a3946e
mperaza <mperaza>
2017-06-04 17:49:42 -0300

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function(){$('.carousel').carousel({interval:false});
+$(document).ready(function(){$('.carousel').carousel({interval:1500});
 
 /* affix the navbar after scroll below header */
 $('#nav').affix({
@@ -66,47 +66,7 @@ $('.panel-thumbnail>a').click(function(e){
 
 
 
-/* google maps 
-google.maps.visualRefresh = true;
-
-var map;
-function initialize() {
-	var geocoder = new google.maps.Geocoder();
-	var address = $('#map-input').text(); 
-	var mapOptions = {
-    	zoom: 15,
-    	mapTypeId: google.maps.MapTypeId.ROADMAP,
-     	scrollwheel: false
-	};
-	map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
-	
-  	if (geocoder) {
-      geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
-          map.setCenter(results[0].geometry.location);
-
-            var infowindow = new google.maps.InfoWindow(
-                {
-                  content: address,
-                  map: map,
-                  position: results[0].geometry.location,
-                });
-
-            var marker = new google.maps.Marker({
-                position: results[0].geometry.location,
-                map: map, 
-                title:address
-            }); 
-
-          } else {
-          	alert("No results found");
-          }
-        }
-      });
-	}
-}*/
-//google.maps.event.addDomListener(window, 'load', initialize);
+// google maps 
 
 /* end google maps */
 
